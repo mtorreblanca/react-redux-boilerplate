70f7c20770271e75441d0b748eb219f8d9c35ce9
Marcelo Torreblanca <v.marcelo.torreblanca@reddit.com>
2022-03-22 11:02:49 -0600

    Add test boilerplate

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,18 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import configureMockStore from "redux-mock-store";
+import { Provider } from "react-redux";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const mockStore = configureMockStore([]);
+
+describe("renders learn react link", () => {
+  it("Render App", () => {
+    render(
+      <Provider store={mockStore()}>
+        <App />
+      </Provider>
+    );
+    const linkElement = screen.getByText(/learn react/i);
+    expect(linkElement).toBeInTheDocument();
+  });
 });
